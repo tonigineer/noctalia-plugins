0bd0253135089051c3d6b5e8a30ed15d56232b05
Toni <hidden.toni@pm.me>
2026-04-26 08:20:09 +0200

    readme adapted

--- wallcards/src/Utils.js
+++ wallcards/src/Utils.js
@@ -10,17 +10,6 @@ function isImage(fileName, filterImages) {
     return filterImages.indexOf(getExtension(fileName)) !== -1;
 }
 
-// function thumbnailName(fileName, filterVideos) {
-//     return isVideo(fileName, filterVideos) ? fileName.substring(0, fileName.lastIndexOf(".")) : fileName;
-// }
-
-function matchesFilter(fileName, selectedFilter, filterImages, filterVideos) {
-    if (selectedFilter === "all") return true;
-    if (selectedFilter === "images") return isImage(fileName, filterImages);
-    if (selectedFilter === "videos") return isVideo(fileName, filterVideos);
-    return false;
-}
-
 function nameFilters(filterImages, filterVideos) {
     return (filterImages || []).concat(filterVideos || []).map((ext) => "*." + ext);
 }
